d08622a213bf3acbdffae23a566e422d794ef3a5
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2024-12-03 09:29:30 -0300

    aulas 1,2 e 3

---
+++ JavaScript_3/Obtendo-Elementos/elementos.js
@@ -0,0 +1,14 @@
+function show() {
+    const contactList = document.getElementById('contact-list')
+    console.log(contactList)
+
+    const listElementes = document.getElementsByTagName('li')
+    console.log(listElementes)
+
+    const contactInputs = document.getElementsByClassName(contactInputs)
+    console.log(contactInputs)
+
+    const contacts = document.querySelectorAll('#contact-input > li > label')
+    console.log(contacts)
+    contacts.forEach
+}
